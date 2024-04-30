880b2624ee9a7bfffeddda429d72b17ee57f298b
venusgalstar <venusgalstar@gmail.com>
2024-04-30 16:00:38 +0200

    update

--- src/components/header.js
+++ src/components/header.js
@@ -13,7 +13,9 @@ function Header() {
                     <li><a href="https://laughcoin.io/#">Home</a></li>
                     <li><a href="https://laughcoin.io/#about">About</a></li>
                     <li><a href="https://laughcoin.io/#tokenomics">Tokenomics</a></li>
-                    <li><a href="https://magiceden.io/marketplace/9dKKUXs4n7teqDL1ZKngP3evYxPjia5AJoQMdMCJTAC8">NFT Collection</a></li>
+                    <li><a href="https://magiceden.io/marketplace/9dKKUXs4n7teqDL1ZKngP3evYxPjia5AJoQMdMCJTAC8" target="_blank">NFT Collection</a></li>
+                    <li><a href="https://staking.laughcoin.io" target="_blank">Staking</a></li>
+                    <li><a href="https://launchpad.laughcoin.io" target="_blank">Launchpad</a></li>
                 </ul>
             </div>
             <div className="header-buy">
